e6db1ec92750a662f6ae798cee2a18f7a37ca6de
hatankokka <hatan.country@gmail.com>
2025-12-07 00:34:17 +0900

    Update HistoricalBordersPlugin.cs

--- src/HistoricalBordersPlugin.cs
+++ src/HistoricalBordersPlugin.cs
@@ -1,1 +1,19 @@
+using YukkuriMovieMaker.Plugin.Shape;
+using YukkuriMovieMaker.Project;
+
+namespace HistoricalBordersPlugin
+{
+    internal class HistoricalBordersPlugin : IShapePlugin
+    {
+        public bool IsExoShapeSupported => false;
+        public bool IsExoMaskSupported => false;
+
+        public string Name => "歴史国境（Historical Borders）";
+
+        public IShapeParameter CreateShapeParameter(SharedDataStore? sharedData)
+        {
+            return new HistoricalBordersParameter(sharedData);
+        }
+    }
+}
 
