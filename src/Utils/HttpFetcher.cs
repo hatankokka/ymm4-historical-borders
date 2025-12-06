c101dc196bf21e919dea4144b6b5aede13eaa83e
hatankokka <hatan.country@gmail.com>
2025-12-07 01:54:26 +0900

    Update HttpFetcher.cs

--- src/Utils/HttpFetcher.cs
+++ src/Utils/HttpFetcher.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,23 +5,11 @@ namespace HistoricalBordersPlugin.Utils
 {
     internal static class HttpFetcher
     {
-        private static readonly HttpClient _client = new HttpClient()
-        {
-            Timeout = TimeSpan.FromSeconds(10)
-        };
+        private static readonly HttpClient _client = new HttpClient();
 
-        /// <summary>
-        /// URL から文字列（UTF-8）を取得する
-        /// （失敗時は例外を投げる）
-        /// </summary>
         public static async Task<string> GetStringAsync(string url)
         {
-            using var resp = await _client.GetAsync(url).ConfigureAwait(false);
-
-            resp.EnsureSuccessStatusCode();
-
-            return await resp.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return await _client.GetStringAsync(url);
         }
     }
 }
-
